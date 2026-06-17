804eed7c8aab76d9e6498358f57a4f71f1297d70
nidal_sentoke <nidalakhmedjanov@gmail.com>
2026-06-18 03:11:00 +0500

    first iteration

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 const (
@@ -11,30 +13,73 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		return []int{}
+	}
+
+	elms := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		elms[i] = rand.Int()
+	}
+
+	return elms
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+
+	max := data[0]
+	for _, v := range data {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	if len(data) < CHUNKS*2 || data == nil {
+		return maximum(data)
+	}
+
+	maxValues := make([]int, 8)
+	var chunkSize = len(data) / CHUNKS
+
+	for i := 0; i < CHUNKS; i++ {
+		begin := i * chunkSize
+		end := begin + chunkSize
+
+		if end > len(data) || i == CHUNKS-1 {
+			end = len(data)
+		}
+
+		maxValues = append(maxValues, maximum(data[begin:end]))
+	}
+
+	return maximum(maxValues)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	elms := generateRandomElements(SIZE)
 
+	now := time.Now()
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	max := maximum(elms)
+	elapsed := time.Since(now)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Microseconds())
 
 	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	now = time.Now()
+	max = maxChunks(elms)
+	elapsed = time.Since(now)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Microseconds())
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,61 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+	m := map[int]int{
+		0:    0,
+		-1:   0,
+		-100: 0,
+		1:    1,
+		100:  100,
+	}
+
+	for arg, exp := range m {
+		slice := generateRandomElements(arg)
+		assert.Len(t, slice, exp)
+	}
+
+	isSame := true
+	slice := generateRandomElements(100)
+	for i := 1; i < len(slice); i++ {
+		if slice[0] != slice[i] {
+			isSame = false
+			break
+		}
+	}
+	assert.False(t, isSame, "slice elements should be different")
+}
+
+func TestMaximum(t *testing.T) {
+	m := map[int][]int{
+		0:       []int{},
+		2:       []int{2},
+		3:       []int{1, 2, 3},
+		5:       []int{3, 4, 5, 2, 1, 0},
+		10:      []int{9, 1, 4, 10, 3, 5},
+		-1:      []int{-10, -100, -34, -1, -34},
+		9991243: []int{100, 341, 412345, 12342, 9991243, 0, 55544, 8345},
+		//24
+		23: []int{
+			0, 1, 2, 3, 4, 5, 6, 7,
+			8, 9, 10, 11, 12, 13, 14, 15,
+			16, 17, 18, 19, 20, 21, 22, 23,
+		},
+		//16
+		15: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+		//17
+		16: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+		//18
+		17: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17},
+	}
+
+	assert.Equal(t, 0, maximum(nil))
+	for exp, sl := range m {
+		assert.Equal(t, exp, maximum(sl))
+	}
+}
